c76b025f941c25b6dc6335101346e68010cb53d6
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-10-11 15:02:32 +0700

    now

---
+++ frontend/client-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@material-tailwind/react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className=" text-blue-300"> Hello world</div>
+    </div>
+  );
+}
+
+export default App;
