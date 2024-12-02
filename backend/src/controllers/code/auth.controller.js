4880e7bc3d4f33f9bebde62e6b2546b324d72b69
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-12-02 09:53:58 +0700

    now

--- backend/src/controllers/code/auth.controller.js
+++ backend/src/controllers/code/auth.controller.js
@@ -1,11 +1,19 @@
+//* ----------------- User controller ---------------------
+
 //TODO: Register user
 
 //TODO: Login user
 
 //TODO: Logout user
 
+//TODO: Get a ser information
+
+//* ----------------- User controller ---------------------
+
 //TODO: Register employee or manager
 
 //TODO: Login employee or manager
 
 //TODO: Logout employee or manager
+
+//TODO: Get all users
