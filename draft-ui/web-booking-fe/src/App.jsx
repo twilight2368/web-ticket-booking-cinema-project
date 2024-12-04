d8dffe55d58446b1f63bd0be6884bbcdfc7f2430
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-12-04 13:46:46 +0700

    now

--- draft-ui/web-booking-fe/src/App.jsx
+++ draft-ui/web-booking-fe/src/App.jsx
@@ -1,10 +1,21 @@
-import { Button } from "@material-tailwind/react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Button>Hello world</Button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*">
+            <Route index element={<>Hello world</>} />
+            <Route path="*" element={<>Not found</>} />
+          </Route>
+          <Route path="/admin/*">
+            <Route index element={<>Hello admin</>} />
+            <Route path="*" element={<>Not found</>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- draft-ui/web-booking-fe/src/main.jsx
+++ draft-ui/web-booking-fe/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "@material-tailwind/react";
+// Supports weights 200-900
+import "@fontsource-variable/nunito";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
