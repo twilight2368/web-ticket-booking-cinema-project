d8baa28de845f7d261a9be134513aaf6767cb970
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-12-04 13:14:50 +0700

    now

--- draft-ui/web-booking-fe/src/main.jsx
+++ draft-ui/web-booking-fe/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ThemeProvider } from "@material-tailwind/react";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
