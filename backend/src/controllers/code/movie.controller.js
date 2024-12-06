c8fe0fa83e09d7a0c438890e0b334f0019f6f451
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-12-06 09:29:18 +0700

    now

--- backend/src/controllers/code/movie.controller.js
+++ backend/src/controllers/code/movie.controller.js
@@ -1,5 +1,6 @@
 const MovieModel = require("../../models/database/Movie"); // Assuming the model is located here
-
+const ShownModel = require("../../models/database/Show");
+const timeZoneUtil = require("../../utils/helpers/time-zone");
 //TODO: Create a new movie
 const createMovie = async (req, res, next) => {
   try {
@@ -119,7 +120,42 @@ const deleteMovie = async (req, res, next) => {
 
 //todo: Get all movies what will be shown on today and the next 2 days
 const getMovieBeingShown = async (req, res, next) => {
-  
+  try {
+    //TODO: Get the current date in server's timezone (UTC+7)
+    const today = timeZoneUtil.getCurrentTimeInServerZone();
+
+    //TODO: Calculate the start and end of the date range (today to next 2 days)
+    const startDateUtc = timeZoneUtil.getStartOfDayInUtc(today); // Start of today in UTC
+    const endDateUtc = timeZoneUtil.getEndOfDayInUtc(
+      today.clone().add(2, "days")
+    ); //TODO: End of next 2 days in UTC
+
+    //TODO: Query to find shows within the specified range
+    const shows = await ShowModel.find({
+      date_show: {
+        $gte: startDateUtc.toDate(),
+        $lte: endDateUtc.toDate(),
+      },
+    }).populate("movie_id"); //TODO: Populate movie details
+
+    //TODO: Extract unique movies being shown in the retrieved shows
+    const uniqueMovies = new Map();
+    shows.forEach((show) => {
+      const movie = show.movie_id;
+      if (!uniqueMovies.has(movie._id)) {
+        uniqueMovies.set(movie._id, movie);
+      }
+    });
+
+    //TODO: Respond with the list of unique movies
+    res.status(200).json({
+      status: 200,
+      data: Array.from(uniqueMovies.values()),
+    });
+  } catch (error) {
+    console.error("Error fetching movies being shown:", error);
+    res.status(500).json({ error: "Failed to fetch movies being shown." });
+  }
 };
 
 module.exports = {
@@ -128,4 +164,6 @@ module.exports = {
   getMovieById,
   updateMovie,
   deleteMovie,
+
+  getMovieBeingShown,
 };
