037a123edf7b6b5a4dec5e068cfd50a5babb9673
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-12-06 10:02:43 +0700

    now

--- backend/src/routes/api.routes.js
+++ backend/src/routes/api.routes.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+
+//TODO: import controllers
+const {
+  getMovieBeingShown,
+  getMovieAboutBeingShown,
+} = require("../controllers/code/movie.controller");
+
 //todo: ----------------------- APP ROUTES --------------------------------------
 router.get("/", (req, res, next) => {
   return res.json({
@@ -8,6 +15,9 @@ router.get("/", (req, res, next) => {
 });
 
 //todo: Get all movies what will be shown on today and the next 2 days
-router.get("/now-movies",);
+router.get("/movies-today", getMovieBeingShown);
+
+//todo: Get all movies what that release date in from the next 3 days and a  month later
+router.get("/movies-about-release", getMovieAboutBeingShown);
 
 module.exports = router;
